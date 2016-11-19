d2f9c213c84d82eaa3d561f4ec9cba53c7d61315
Thomas J. Leeper <thosjleeper@gmail.com>
2016-11-19 14:20:52 +0000

    add auth_details() func

--- R/get_account.R
+++ R/get_account.R
@@ -3,7 +3,7 @@
 #' @description Retrieve IAM Account Details. This is useful as a \dQuote{hello world!} test.
 #' @template dots
 #' @return A list containing various account details.
-#' @details \code{get_account} returns a list of account details. \code{credential_report} generates and/or retrieves a credential report.
+#' @details \code{get_account} returns a list of account details. \code{credential_report} generates and/or retrieves a credential report. \code{auth_details} returns a list of group, user, role, and policy details.
 #' @export
 get_account <- function(...) {
     query <- list(Action = "GetAccountSummary")
@@ -22,4 +22,31 @@ credential_report <- function(...) {
         message(desc)
     }
     desc
+}
+
+#' @rdname get_account
+#' @param type An optional character string specifying one or more types of reports to return.
+#' @template n
+#' @template marker
+#' @template dots
+#' @export
+auth_details <- function(type, n, marker, ...) {
+    query <- list(Action = "GetAccountAuthorizationDetails")
+    if (!missing(type)) {
+        type <- match.arg(type, several.ok = TRUE)
+        a <- as.list(type)
+        names(a) <- paste0("Filter.member.",1:length(a))
+        query <- c(query, a)
+    }
+    if (!missing(marker)) {
+        query$Marker <- marker
+    }
+    if (!missing(n)) {
+        if (!n %in% 1:1e3) {
+            stop("'n' must be in 1:1000")
+        }
+        query$MaxItems <- n
+    }
+    out <- iamHTTP(query = query, ...)
+    out[["GetAccountAuthorizationDetailsResponse"]][["GetAccountAuthorizationDetailsResult"]]
 }
